602bc6f6fd8edc97f9d456e3bb94b17c1e279f42
hyojin <jihojhi@gmail.com>
2023-07-10 21:15:45 +0900

    :technologist: mini blog practice

--- src/Component/list/CommentList.jsx
+++ src/Component/list/CommentList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { styled } from "styled-components";
+import styled from "styled-components";
 import CommentListItem from "./CommentListItem";
 
 const Wrapper = styled.div`
@@ -16,14 +16,12 @@ const Wrapper = styled.div`
 `;
 
 function CommentList(props) {
-    const {comments} = props;
+    const { comments } = props;
 
     return (
         <Wrapper>
             {comments.map((comment, index) => {
-                return (
-                    <CommentListItem key={comment.id} comment={comment} />
-                );
+                return <CommentListItem key={comment.id} comment={comment} />;
             })}
         </Wrapper>
     );
--- src/Component/list/CommentListItem.jsx
+++ src/Component/list/CommentListItem.jsx
@@ -3,29 +3,31 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
     width: calc(100% - 32px);
-    padding: 16px;
+    padding: 8px 16px;
     display: flex;
     flex-direction: column;
-    align-items: felx-start;
+    align-items: flex-start;
     justify-content: center;
     border: 1px solid grey;
-    border-redius: 8px;
+    border-radius: 8px;
     cursor: pointer;
     background: white;
-    :hovor {
+    :hover {
         background: lightgrey;
     }
 `;
 
 const ContentText = styled.p`
-    font-size: 14px;
+    font-size: 16px;
+    white-space: pre-wrap;
 `;
 
 function CommentListItem(props) {
-    const {comment} = props;
+    const { comment } = props;
+
     return (
         <Wrapper>
-            <ContentText>{comment.title}</ContentText>
+            <ContentText>{comment.content}</ContentText>
         </Wrapper>
     );
 }
